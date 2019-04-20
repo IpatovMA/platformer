2418652ab0f30dd510a8ba1a78f2aeb5640a161e
Mikhail <lolpie24@yandex.ru>
2019-04-20 03:01:10 +0300

    добавлен таймер, детекторы касаний, золото, смерть

--- main.lua
+++ main.lua
@@ -11,73 +11,43 @@ math.randomseed(os.time())
 
 local physics = require("physics")
 physics.start()
-physics.setGravity(0,12)
+--physics.setGravity(0,12)
 
-
+local module = require("module")
 
 --построение карты
+-- local background = display.newImageRect( "background.png", display.actualContentWidth, display.actualContentHeight )
+-- background.x = display.contentCenterX
+-- background.y = display.contentCenterY
 local fileName = 'map.txt'
-local filePath = system.pathForFile(fileName)
-local file = io.open(filePath, "r")
-
 local block_size=50
 local map_width = 80
 local map_height = 15
+local map = mapbild (fileName,block_size,map_width,map_height)
 
-local x = -block_size/2
-local y = -block_size/2
-local map = {}
-
- for  i=1,map_height do
-  map[i]={}
-  local line = file:read("*l")
-    for j=1,map_width do
-      map[i][j]=string.match(line, "%a", j)
-    end
+--таймер
+local sec = 0
+local level_time = display.newText(sec, display.contentCenterX, 20, native.systemFont, 40 )
+ level_time:setFillColor(math.random(),math.random(),math.random())
+local timer_show = function()
+  sec = sec +1
+   level_time.text = sec
 end
-io.close( file )
-
-for i=1,map_height do
-  for j=1,map_width do
-    if map[i][j] == "z" then
-      map[i][j] = {
-        rect=display.newRect(j*block_size+x,i*block_size+y,block_size,block_size),
-        id = "air"--пустые блоки
-      }
-      camera:add(map[i][j].rect,1)
-    end
-    if map[i][j] == "B" then
-      map[i][j] = {
-        rect=display.newRect(j*block_size+x,i*block_size+y,block_size,block_size),
-        id = "block"--твердые блоки
-      }
-      map[i][j].rect:setFillColor(math.random(),math.random(),math.random())
-      physics.addBody(map[i][j].rect,"static",{bounce = 0,friction = 1.0})
-      camera:add(map[i][j].rect,1)
-    end
-    if map[i][j] == "G" then
-      map[i][j] = {
-        rect=display.newRect(j*block_size+x,i*block_size+y,block_size,block_size),
-        id = "gold"--голда
-      }
-      map[i][j].rect:setFillColor(0,1,0)
-      physics.addBody(map[i][j].rect,"static",{bounce = 0,friction = 1.0})
-      camera:add(map[i][j].rect,1)
-    end
-  end
-end
---угол почему то сместился
---local conerX,conerY = left_top(map[1][1].rect)
+timer.performWithDelay( 1000, timer_show ,-1 )
 
 --пресонаж
-local player = display.newRect(0,0,32,64)
-player.x = 115
-player.y = display.contentCenterY
-player:setFillColor(1,1,0)
+local start_player_position_x = 100
+local start_player_position_y = display.contentCenterY
+
+local player = display.newRect(start_player_position_x,start_player_position_y,32,64)
+player:setFillColor(1,0.8,0)
 camera:add(player, 1)
 physics.addBody(player,"dynamic",{density=3.0,bounce = 0,friction = 1.0})
 player.isFixedRotation = true
 
+local gold = {count = 0}
+gold.show = display.newText(gold.count, display.contentCenterX + display.actualContentWidth/2.2, 20, native.systemFont, 40)
+gold.show:setFillColor(1,1,0)
 
 --камера
 map_height=map_height*block_size
@@ -86,54 +56,77 @@ camera:setFocus(player)
 camera:track()
 camera:setBounds(display.contentCenterX, map_width - display.contentCenterX, display.contentCenterY/2, display.contentCenterY*1.5)
 
+--детекторы касаний
+local function onGround (obj)
+local i=math.ceil((bott_y(obj)+1)/block_size)
+  for j=(math.ceil((left_x(obj))/block_size)),(math.ceil((rigth_x(obj))/block_size)) do
+   if map[i][j].id=="block" then
+     return true
+   end
+  end
+  return false
+end
+
+local function onSpikes (obj)
+local i=math.ceil((bott_y(obj)+1)/block_size)
+  for j=(math.ceil((left_x(obj))/block_size)),(math.ceil((rigth_x(obj))/block_size)) do
+   if map[i][j].id=="spike" then
+     return true
+   end
+  end
+  return false
+end
+
+local function catchGold (obj)
+for i = math.ceil((top_y(obj)-1)/block_size),math.ceil((bott_y(obj)+1)/block_size) do
+  for j=(math.ceil((left_x(obj)-1)/block_size)),(math.ceil((rigth_x(obj)+1)/block_size)) do
+   if map[i][j].id=="gold" then
+     map[i][j].id="air"
+     physics.removeBody(map[i][j].rect)
+     map[i][j].rect.alpha = 0
+     gold.count=gold.count+1
+     gold.show.text=gold.count
+   end
+  end
+end
+  return false
+end
+
+local function eventChecker ()
+  if onSpikes(player) then
+    player_death()
+  end
+  if catchGold(player)then
+
+  end
+end
+
+Runtime:addEventListener( "enterFrame", eventChecker )
+--смерть персонажа, перезагрузка уровня
+function player_death ()
+  player.x = start_player_position_x
+  player.y = start_player_position_y
+  sec=0
+  gold.count=0
+  gold.show.text=gold.count
+end
+
 --управение
- local up_flag = false
- local down_flag = false
- local left_flag = false
- local rigth_flag = false
-
-
- local function onGround (obj)
-   local flag = false
- local i=math.ceil((bott_y(obj)+1)/block_size)
-    for j=(math.ceil((left_x(obj))/block_size)),(math.ceil((rigth_x(obj))/block_size)) do
-
-     if map[i][j].id=="block"   then
- --       print(math.ceil((left_x(obj)+4)/block_size),math.ceil((rigth_x(obj)-4)/block_size))
- -- print(((bott_y(obj)-block_size/2+1)/block_size)+1)
- --  print(map[i][j].id)
- -- print(rigth_x(obj),(left_x(map[i][j].rect)))
-       return true
-     end
-    end
-return false
-    end
-
--- local function onGroundcollision (self, event)
---    local i=math.floor((bott_y(self)-display.contentCenterY/1.5-block_size/2+1)/block_size)+1
---       for j=math.ceil((left_x(self))/block_size),math.ceil(rigth_x(self)/block_size) do
---         if map[i][j].id=="block"   then
---              if event.contact.isTouching(map[i][j].rect) then
---              print(i,j)
---
---              return true
---           else return false end
---       end end
--- end
---
--- player.collision = onGroundcollision
--- player:addEventListener("collision")
+local up_flag = false
+local down_flag = false
+local left_flag = false
+local rigth_flag = false
 
 local walkspeed = 300
 local jampspeed = 350
-local verticalspeed = 0
 
 local function walkplayer (event)
   local vx,vy = player:getLinearVelocity()
  if (rigth_flag) then
    if not(onGround(player)) then
      player:setLinearVelocity(walkspeed/1.3, vy )
-   else player:setLinearVelocity(walkspeed, vy )end
+   else player:setLinearVelocity(walkspeed, vy )
+   end
  end
  if (left_flag) then
    if not(onGround(player)) then
@@ -141,25 +134,13 @@ local function walkplayer (event)
    else player:setLinearVelocity(-walkspeed, vy )end
  end
  if up_flag and (onGround(player)) then
-    --  player.y = player.y - 40
-  --  player:applyLinearImpulse(0,-4,player.x,player.y)
   player:setLinearVelocity(vx/1.3, -jampspeed )
---  verticalspeed=-jampspeed
  end
  if (down_flag) then
  end
--- print(onGround(player))
---onGround(player)
- -- if not(rigth_flag) and not(left_flag) then
- --    player:setLinearVelocity(0,vy )
+ -- if not(rigth_flag) and not(left_flag) and not(up_flag) then
+ --    player:setLinearVelocity(0)
  -- end
-end
-
-local function verticalmovement()
-    if verticalspeed<0 then
-        verticalspeed=verticalspeed/2
-        print(verticalspeed)
-    end
 end
 
 local function keyboardcontrol(event)
@@ -180,8 +161,6 @@ local function keyboardcontrol(event)
  else down_flag=false
  end
 end
-local module = require("module")
 
 Runtime:addEventListener("key", keyboardcontrol)
 Runtime:addEventListener( "enterFrame", walkplayer )
-player:addEventListener("enterFrame",verticalmovement)
--- module.lua
+++ module.lua
@@ -1,28 +1,4 @@
---координаты разных точке объекта
-
-rigth_top = function (obj)
-  local x = obj.x + obj.width/2
-  local y = obj.y - obj.height/2
-  return x,y
-end
-
- left_top = function (obj)
-  local x = obj.x - obj.width/2
-  local y = obj.y - obj.height/2
-  return x,y
-end
-
- rigth_bott = function (obj)
-  local x = obj.x + obj.width/2
-  local y = obj.y + obj.height/2
-  return x,y
-end
-
- left_bott = function (obj)
-  local x = obj.x - obj.width/2
-  local y = obj.y + obj.height/2
-  return x,y
-end
+--координаты
 --x
 rigth_x = function (obj)
   local x = obj.x + obj.width/2
@@ -44,15 +20,78 @@ bott_y = function (obj)
   return y
 end
 --сетка
-for i=0,25 do
-  local x = display.newRect(i*50,display.contentCenterY,5,display.actualContentHeight)
-  camera:add(x,1)
-  x:setFillColor(0,0,1)
+-- for i=0,25 do
+--   local x = display.newRect(i*50,display.contentCenterY,5,display.actualContentHeight)
+--   camera:add(x,1)
+--   x:setFillColor(0,0,1)
+-- end
+-- for i=0,display.actualContentHeight/50 do
+--   local x = display.newRect(display.contentCenterX,i*50,5000,5)
+--     camera:add(x,1)
+--     x:setFillColor(0,0,1)
+-- end
+--построение карты
+function mapbild (fileName,block_size,map_width,map_height)
 
-end
-for i=0,display.actualContentHeight/50 do
-  local x = display.newRect(display.contentCenterX,i*50,5000,5)
-    camera:add(x,1)
-    x:setFillColor(0,0,1)
+  local filePath = system.pathForFile(fileName)
+  local file = io.open(filePath, "r")
 
+
+  local x = -block_size/2
+  local y = -block_size/2
+  local map = {}
+
+   for  i=1,map_height do
+    map[i]={}
+    local line = file:read("*l")
+      for j=1,map_width do
+        map[i][j]=string.match(line, "%a", j)
+      end
+  end
+  io.close( file )
+
+  for i=1,map_height do
+    for j=1,map_width do
+      --пустые блоки
+      if map[i][j] == "z" then
+        map[i][j] = {
+          rect=display.newRect(j*block_size+x,i*block_size+y,block_size,block_size),
+          id = "air"
+        }
+        map[i][j].rect.alpha = 0
+        camera:add(map[i][j].rect,1)
+      end
+      --твердые блоки
+      if map[i][j] == "B" then
+        map[i][j] = {
+          rect=display.newRect(j*block_size+x,i*block_size+y,block_size,block_size),
+          id = "block"
+        }
+        map[i][j].rect:setFillColor(0.37, 0.56, 0.58)
+        physics.addBody(map[i][j].rect,"static",{bounce = 0,friction = 1.0})
+        camera:add(map[i][j].rect,1)
+      end
+      --голда
+      if map[i][j] == "G" then
+        map[i][j] = {
+          rect=display.newRect(j*block_size+x,i*block_size+y,block_size,block_size),
+          id = "gold"
+        }
+        map[i][j].rect:setFillColor(1,1,0)
+        physics.addBody(map[i][j].rect,"static",{bounce = 0,friction = 1.0})
+        camera:add(map[i][j].rect,1)
+      end
+      --шипы, чтобы протестить умирание персонажа
+      if map[i][j] == "A" then
+        map[i][j] = {
+          rect=display.newRect(j*block_size+x,i*block_size+y,block_size,block_size),
+          id = "spike"
+        }
+        map[i][j].rect:setFillColor(1)
+        physics.addBody(map[i][j].rect,"static",{bounce = 0,friction = 1.0})
+        camera:add(map[i][j].rect,1)
+      end
+    end
+  end
+  return map
 end
